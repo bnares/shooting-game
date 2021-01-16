483ca228adcff4b91bfb92f252b3e92db5c3ad9f
Piotr Ostrouch <piotr.ostrouch@gmail.com>
2021-01-16 14:52:21 +0100

    Basic class

--- game.py
+++ game.py
@@ -5,6 +5,16 @@ pygame.init()
 clock = pygame.time.Clock()
 
 
+class Crosshair(pygame.sprite.Sprite):
+    def __init__(self, width,height, pos_x,pos_y, color):
+        super().__init__()
+        self.image = pygame.Surface((width,height))
+        self.image.fill(color)
+        self.rect = self.image.get_rect()
+        self.rect.center = (pos_x, pos_y)
+
+
+
 #Game screen
 
 screen_width = 1000
@@ -16,7 +26,7 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
-            
+
 
     pygame.display.update()
     clock.tick(60)
