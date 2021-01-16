32c806fd1eaba15cad857d0c8a11a4beaa207202
Piotr Ostrouch <piotr.ostrouch@gmail.com>
2021-01-16 15:22:08 +0100

    bg screen

--- game.py
+++ game.py
@@ -21,12 +21,22 @@ screen_width = 1000
 screen_height = 600
 screen = pygame.display.set_mode((screen_width, screen_height))
 
+#background
+
+background = pygame.image.load("bg.png")
+
+
+crosshair = Crosshair(50,50,100,100, (255,255,255))
+
+group = pygame.sprite.Group()
+group.add(crosshair)
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
 
-
+    screen.blit(background, (0,0))
+    group.draw(screen)
     pygame.display.update()
     clock.tick(60)
